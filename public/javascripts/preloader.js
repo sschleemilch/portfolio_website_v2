14e4112ed889815aa0e1a07d780b0762d497b8e8
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2021-05-19 22:04:27 +0200

    Show loader on site link clicking

--- public/javascripts/preloader.js
+++ public/javascripts/preloader.js
@@ -1,3 +1,6 @@
+function showLoader() {
+    document.getElementById("preloader").classList.remove("off")
+}
 
 window.onload = function () {    
     document.getElementById("preloader").classList.add("off");
